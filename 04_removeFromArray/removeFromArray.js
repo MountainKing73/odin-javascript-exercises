db6367802699089322d2671f4bca9d9b4b68e521
James Cummings <jamescummings@gmail.com>
2024-09-12 09:19:57 -0400

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (origArray) {
+  const args = Array.from(arguments);
+  return origArray.filter((element) => !args.includes(element));
 };
 
 // Do not edit below this line
