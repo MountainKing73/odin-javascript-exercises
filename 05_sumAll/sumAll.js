9ef4cd4d97a72716993fb8f314763e62cab4aa47
James Cummings <jamescummings@gmail.com>
2024-09-12 09:26:52 -0400

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
 
+  let from = num1;
+  let to = num2;
+  if (num1 > num2) {
+    from = num2;
+    to = num1;
+  }
+  let total = 0;
+  for (let i = from; i <= to; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
