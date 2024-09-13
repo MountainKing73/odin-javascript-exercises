dbadf93942c6ff60cb41138a9c5838b3cb57ecaf
James Cummings <jamescummings@gmail.com>
2024-09-13 15:12:04 -0400

    initial palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let front = 0;
+  let back = word.length - 1;
 
+  while (back > front) {
+    if (word[front] === word[back]) {
+      front++;
+      back--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
