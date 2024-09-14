ce3e5e7e49ceed7140e1a463cda1779c59afa237
James Cummings <jamescummings@gmail.com>
2024-09-14 11:09:16 -0400

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num < 0) {
+    return "OOPS";
+  }
 
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+
+  let num1 = 0;
+  let num2 = 1;
+  let fib = 0;
+
+  for (let i = 2; i <= num; i++) {
+    fib = num1 + num2;
+    num1 = num2;
+    num2 = fib;
+  }
+
+  return fib;
 };
 
 // Do not edit below this line
