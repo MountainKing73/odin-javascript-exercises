f2d73895fdbc07868475d39b2e5f73c364075ad8
James Cummings <jamescummings@gmail.com>
2024-09-14 11:00:32 -0400

    Completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,8 +2,29 @@ const palindromes = function (word) {
   let front = 0;
   let back = word.length - 1;
 
+  word = word.toLowerCase();
+
   while (back > front) {
-    if (word[front] === word[back]) {
+    let frontLetter = word[front];
+    let backLetter = word[back];
+
+    if (
+      (frontLetter < "a" || frontLetter > "z") &&
+      !(frontLetter >= "0" && frontLetter <= "9")
+    ) {
+      front++;
+      continue;
+    }
+
+    if (
+      (backLetter < "a" || backLetter > "z") &&
+      !(backLetter >= "0" && backLetter <= "9")
+    ) {
+      back--;
+      continue;
+    }
+
+    if (frontLetter === backLetter) {
       front++;
       back--;
     } else {
