5e9ecdc6f67c04bc2be54064ffc855b317f38e10
James Cummings <jamescummings@gmail.com>
2024-09-14 11:26:06 -0400

    Completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const sorted = people.sort((a, b) => {
+    let aAge = a.yearOfDeath - a.yearOfBirth;
+    if (a.yearOfDeath === undefined) {
+      aAge = new Date().getFullYear() - a.yearOfBirth;
+    }
 
+    let bAge = b.yearOfDeath - b.yearOfBirth;
+    if (b.yearOfDeath === undefined) {
+      bAge = new Date().getFullYear() - b.yearOfBirth;
+    }
+
+    return bAge - aAge;
+  });
+
+  return sorted[0];
 };
 
 // Do not edit below this line
